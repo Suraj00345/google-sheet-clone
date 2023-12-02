95eb33d156434fa63388ccc51997b5fa8515c4c8
Suraj00345 <surajkumarmanna2000@gmail.com>
2023-12-02 10:48:55 +0530

    2nd commit

--- functionality.js
+++ functionality.js
@@ -120,3 +120,21 @@ function exportData() {
     link.href = url;
     link.click();
 }
+
+
+const input = document.getElementById('upload');
+const link = document.getElementById('link');
+let objectURL;
+
+input.addEventListener('change', function () {
+  if (objectURL) {
+    // revoke the old object url to avoid using more memory than needed
+    URL.revokeObjectURL(objectURL);  
+  }
+
+  const file = this.files[0];
+  objectURL = URL.createObjectURL(file);
+
+  link.href = objectURL;
+  link.click();
+});
